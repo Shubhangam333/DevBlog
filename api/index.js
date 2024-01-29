2cac34951bf9475241fe52c5d5321392e2b34535
Shubhangam333 <shubhangam27@gmail.com>
2024-01-29 17:56:35 +0530

    create signupapi route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -14,6 +16,11 @@ mongoose
   });
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
