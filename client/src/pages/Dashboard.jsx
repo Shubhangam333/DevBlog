1d3a422b35ae446963d50d5d3a123209b9ef63ef
Shubhangam333 <shubhangam27@gmail.com>
2024-02-08 11:25:44 +0530

    add dashboard overview and comment tabs

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,6 +4,8 @@ import DashSideBar from "../components/DashSideBar";
 import DashProfile from "../components/DashProfile";
 import DashPosts from "../components/DashPosts";
 import DashUsers from "../components/DashUsers";
+import DashComments from "../components/DashComments";
+import DashboardComp from "../components/DashBoardComp";
 
 export default function Dashboard() {
   const location = useLocation();
@@ -27,6 +29,10 @@ export default function Dashboard() {
       {tab === "posts" && <DashPosts />}
       {/* users */}
       {tab === "users" && <DashUsers />}
+      {/* comments  */}
+      {tab === "comments" && <DashComments />}
+      {/* dashboard comp */}
+      {tab === "dash" && <DashboardComp />}
     </div>
   );
 }
